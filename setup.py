81a3c8b221dcf49a86597c942b4331756f617421
Tyler Bovenzi <tylerbovenzi4427@gmail.com>
2022-01-13 14:47:24 -0800

    Modified URL

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Tyler Bovenzi',                   # Type in your name
   author_email = 'Tylerb4427@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/TylerBovenzi',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/TylerBovenzi/SpiController2040/archive/refs/tags/Library.tar.gz',    # I explain this later on
   keywords = ['Nexstream'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'machine'
