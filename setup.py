de5e76039eadb9cb796cb726a0eb1e6d7206dff9
Tyler Bovenzi <tylerbovenzi4427@gmail.com>
2022-01-20 16:34:22 -0800

    Variety

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'Spi2040',         # How you named your package folder (MyLib)
   packages = ['Spi2040'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'RP2040 Modified SPI Library',   # Give a short description about your library
   author = 'Tyler Bovenzi',                   # Type in your name
   author_email = 'Tylerb4427@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/TylerBovenzi',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/TylerBovenzi/Spi2040/archive/refs/tags/v0.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/TylerBovenzi/Spi2040/archive/refs/tags/v0.3.tar.gz',    # I explain this later on
   keywords = ['Nexstream'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'machine'
